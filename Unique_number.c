59bba514b079400be4accf78b18ed9e1fb6e1462
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-04-21 15:29:19 +0530

    #include<stdio.h> #include<string.h> int main() {
        int i,j,t=0;
        char a[10];
        scanf("%[^
    ]s",a);
        for(j=0;j<strlen(a);j++){
            for(i=0;i<strlen(a);i++){
                if(a[i]==a[j]){
                    t++;
                }
            }
        }
        if(t>strlen(a)){
            printf("Not Unique Number ");
        }
        else{
            printf("Unique Number");
        }
        return 0;
    }

---
+++ Unique_number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    
+    int i,j,t=0;
+    char a[10];
+    scanf("%[^
+]s",a);
+    for(j=0;j<strlen(a);j++){
+        for(i=0;i<strlen(a);i++){
+            if(a[i]==a[j]){
+                t++;
+            }
+        }
+    }
+    if(t>strlen(a)){
+        printf("Not Unique Number ");
+    }
+    else{
+        printf("Unique Number");
+    }
+    return 0;
+}
